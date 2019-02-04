9b77de5954a2205b40f1979781be12f8e7db05f2
Emmanuel Lemaistre <manu9576@hotmail.fr>
2019-02-04 22:42:05 +0100

    Creation

---
+++ CommandPattern/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CommandPattern
+
+{
+    /// <summary>
+    /// The 'Receiver' class
+    /// </summary>
+    class Calculator
+    {
+        private int _curr = 0;
+
+        public void Operation(char @operator, int operand)
+        {
+            switch (@operator)
+            {
+                case '+':
+                    _curr += operand; break;
+
+                case '-':
+                    _curr -= operand; break;
+
+                case '*':
+                    _curr *= operand; break;
+
+                case '/':
+                    _curr /= operand; break;
+
+            }
+            Console.WriteLine($"Current value = {_curr,3} (following { @operator} {operand})");
+        }
+    }
+}
---
+++ CommandPattern/Command.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandPattern
+{
+    /// <summary>
+    /// The 'Command' abstract class
+    /// </summary>
+    abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+}
